c3811c597f37f3f5fe4ffddf3d2642467627ecc8
Farhan Al Egal <farhan.eizo.al.egal@nmbu.no>
2022-02-10 23:12:24 +0100

    while løkke nesten er klar.

--- BacheloroppgaveGr10.m
+++ BacheloroppgaveGr10.m
@@ -5,33 +5,33 @@
 % tMax - Hvor lenge simuleringa skal vare
 % N - antall punkter i x - for plottinga
 % Landskap
-f = @(x) x.^2;
+f =@(x) x.^2;
 fig = figure(1);
 xlabel("X-axis",'fontsize',16,'color','b');
 ylabel("Y-axis",'fontsize',16,'color','b');
 title("Simulating of sliding",'fontsize',16,'color','r');
 
 % X-posisjon som funksjon av tid
-x0 = 5;             % Startposisjon
+x0 = 4.8;             % Startposisjon
 Vx0 =0;                %Start av farten
 % Oppløsninga i tid - steglengda - og varigheten av simuleringa
-dt = 0.0025;
-tMax=20;
+dt = 0.01;
+tMax=10;
 t=0;                % Start-tid
 % mot venstre
 W =98.1;
-h = 0.01;
-g = 9.81;
+h = 0.001;
+g = 9.81; 
+M =0.025;
 D=(f(X+h)-f(X-h))/(2*h);
 B =-atan(D);
 N=W*cos(B);                 % Normalkraft
 S=M*N;
-BV = true;
-M =0.25;                   %Friksjonskoeffisienten (0-1)
+presisjon = 1e-6;
+Vx = 0.1;                  %Steglengde (blir endra inne i løkka)
 % Vektor med x-verdier
-Na=100;              % antall punkter
-xVektor = linspace(-1.*x0,1.*x0,Na);   % Vektor med x-verdier
-
+Na=300;              % antall punkter
+xVektor = linspace(-5,5,Na);   % Vektor med x-verdier
 % Lager plott
 plot(xVektor,f(xVektor),'k-','linewidth',2) % Plotter landskap
 hold on
@@ -40,12 +40,14 @@ yVerdi = f(xVerdi);
 % Ploter posisjonen til objektet (rød stjerne)
 pl = plot(xVerdi,yVerdi,'rx','linewidth',10);    
 hold off
- Vx = Vx0;
  X =x0;
- for t=0:dt:tMax
-%while abs(Vx)>1e-6 || abs(S)<W*sin(B)    % Looper over alle tidspunktene
-if BV
-      %nx = Normalkraft(h,X,f); 
+ % For video
+v = VideoWriter ('test.avi');
+open (v); 
+% Løkke som går over tidspunktene
+indeks=1;               % Innfører indeks som teller iterasjoner
+ while abs(Vx)>presisjon || abs(S)<W*sin(B)   % Looper over alle tidspunktene
+
       ax=Akselerasjon(Vx,X,h,f,M,g);
       VxHatt=Vx+ax*dt/2;
       XHatt=X+Vx*dt/2;
@@ -54,16 +56,22 @@ if BV
       X=X+Vx*dt;
       Y=f(X);
       F=(f(X+h)-2*f(X)+f(X-h))/(h^2);
-else
-   %abs(Vx)<1e-6;
-   %abs(S)>W*sin(B);
-         %disp('Utanfor området.')      % Skriv til skjerm
-          %hold off                      % Sluttar å "spare på" plott
-      
-end
+
+  if abs(Vx)<presisjon && abs(S)>W*sin(B)
+    break
+  end
    % Oppdaterer data til plotting
      set(pl,'xdata',X);
      set(pl,'ydata',Y);
 
   drawnow               % Oppdaterer selve plottet
+    % Sparer på hver 5. frame til video
+  if mod(indeks,5)==0 
+    % Spare på "frame" til filmen
+    frame=getframe(gcf);
+    writeVideo(v, frame);
+  end
+  indeks=indeks+1;      % Oppdaterer indeks  
 end
+% Lukker video-fila
+close(v)
