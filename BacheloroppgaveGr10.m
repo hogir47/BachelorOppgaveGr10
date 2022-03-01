91f9e4e684a73f247e8954162825e12b6ddb9685
Farhan Al Egal <farhan.eizo.al.egal@nmbu.no>
2022-03-01 23:28:08 +0100

    Nytt landskap!

--- BacheloroppgaveGr10.m
+++ BacheloroppgaveGr10.m
@@ -5,20 +5,23 @@
 % tMax - Hvor lenge simuleringa skal vare
 % Na - antall punkter i x - for plottinga
 % Landskap
-f =@(x) x.^2;
+%f =@(x) x.^2;
+f =@(x) x.^2/4 - 4 * cos(x-1);
 fig = figure(1);
 xlabel("X-axis",'fontsize',16,'color','b');
 ylabel("Y-axis",'fontsize',16,'color','b');
 title("Simulating of sliding",'fontsize',16,'color','r');
 
 % X-posisjon som funksjon av tid
-x0 = 1;             % Startposisjon
+x0 = -2;             % Startposisjon
 Vx0 =0;                %Start av farten
 % Oppløsninga i tid - steglengda - og varigheten av simuleringa
 dt = 0.005;
 tMax=10;
 t=0;                % Start-tid
 % mot venstre
+xMin=-10;
+xMax=10;
 X =x0;
 h = 0.001;
 g = 9.81; 
@@ -43,6 +46,7 @@ hold off
 % Løkke som går over tidspunktene
 %indeks=1;               % Innfører indeks som teller iterasjoner
 %while abs(Vx) > Vmin || D > M
+ii=1;
 Xstopp= 0;
 XstoppNy= 10*presisjon;
 while abs(Xstopp-XstoppNy)>presisjon
@@ -63,17 +67,20 @@ while abs(Vx) > Vmin || D > M   % Looper over alle tidspunktene
       Y=f(X);
       F=(f(X+h)-2*f(X)+f(X-h))/(h^2);
    % Oppdaterer data til plotting
+     ii=ii+1;
      set(pl,'xdata',X);
      set(pl,'ydata',Y);
 
   drawnow               % Oppdaterer selve plottet
     % Sparer på hver 5. frame til video
   %if mod(indeks,5)==0 
+  if mod(ii,5)==0;
     % Spare på "frame" til filmen
     %frame=getframe(gcf);
     %writeVideo(v, frame);
   %end
   %indeks=indeks+1;      % Oppdaterer indeks  
+  end
  end
   XstoppNy=X
   M=M/2
