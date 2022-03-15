eeb5d495338f403922e9cd0c54ead7c7f37019b9
Farhan Al Egal <farhan.eizo.al.egal@nmbu.no>
2022-03-16 00:57:24 +0100

    Appen er nesten ferdig.

--- Akselerasjon.m
+++ Akselerasjon.m
@@ -1,12 +1,11 @@
 function A = Akselerasjon(Vx,X,h,f,M,g)
-
 % Euler's Metode
 %Funksjon av akselerasjon inneholder variabler
-
 % D er første dervet av landscap
 D=(f(X+h)-f(X-h))/(2*h);
 B =-atan(D);
 F=(f(X+h)-2*f(X)+f(X-h))/(h^2);
-A= ( sin(B)-sign(Vx)*M*cos(B))*(g+F*Vx^2)/...
-((1 + sign(Vx)*M*D)*cos(B)+(sign(Vx)*M-D)*sin(B));
+CheckSign=sign(g+F*Vx^2);
+A= ( sin(B)-CheckSign*sign(Vx)*M*cos(B))*(g+F*Vx^2)/...
+  ((1 + sign(Vx)*M*D)*cos(B)+(sign(Vx)*M-D)*sin(B));
        
