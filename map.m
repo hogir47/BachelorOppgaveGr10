d575b17187e94927f29abc21de2f7cc39635e593
Farhan Al Egal <farhan.eizo.al.egal@nmbu.no>
2022-03-24 15:42:52 +0100

    Map og x0,Vx0 testing

--- map.m
+++ map.m
@@ -1,17 +1,17 @@
 function map()
 x0ind=0;
-for x0 =-10:5:10
+for x0 =-10:1:10
         x0ind=x0ind+1;
         tic
         Vx0ind=0;
-        for Vx0 =-10:5:10
+        for Vx0 =-10:1:10
             Vx0ind=Vx0ind+1;
             Mat(x0ind,Vx0ind)=MainFunksjonIkkePlot(x0,Vx0);
             toc;
         end
 end
-X =x0;
-    V =Vx0;
+X =-10:1:10;
+    V =-10:1:10;
 surf(X,V,Mat);
 colormap(jet)
     colorbar;
