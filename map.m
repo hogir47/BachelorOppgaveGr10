b02811f74d5d9c5c320e8802b8b37a366400dcd0
Farhan Al Egal <farhan.eizo.al.egal@nmbu.no>
2022-04-03 00:41:23 +0200

    Adding dx,dvx,xmax og vmax i appen.

--- map.m
+++ map.m
@@ -1,20 +1,27 @@
-function map(dx,f)
+function map(dx,dvx,xmax,vmax,f)
 x0ind=0;
-for x0 =-10:dx:10
+for x0 =-xmax:dx:xmax
         x0ind=x0ind+1;
         tic
         Vx0ind=0;
-        for Vx0 =-10:dx:10
+        for Vx0 =-vmax:dvx:vmax
             Vx0ind=Vx0ind+1;
             Mat(x0ind,Vx0ind)=MainFunksjonIkkePlot(x0,Vx0,f);
             toc;
         end
 end
+
 figure(f);
-X =-10:dx:10;
-V =-10:dx:10;
-pcolor(X,V,Mat);
-colormap(jet)
+
+Npts_x=((2*xmax)/dx)+1;
+Npts_v=((2*vmax)/dvx)+1;
+
+Xvector =linspace(-xmax,xmax,Npts_x);
+Vvector =linspace(-vmax,vmax,Npts_v);
+
+surf(Vvector,Xvector,Mat);
+colormap(jet);
 colorbar;
+
 end
    
